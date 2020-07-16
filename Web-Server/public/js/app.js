bc11d0b5050988569617258e36831511215e3070
Anjali1999 <anjali24.7goyal@gmail.com>
2020-07-16 20:01:24 +0530

    Adding frontend and making API calls
    Ensuring that the website communicates correctly with NodeJS. It takes a location as the form input and displays respective weather data when a valid address is provided.

---
+++ Web-Server/public/js/app.js
@@ -0,0 +1,27 @@
+console.log('Client side js loaded.')
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const p1 = document.querySelector('#msg1')
+const p2 = document.querySelector('#msg2')
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const location = search.value
+
+    p1.textContent = 'Loading...'
+    p2.textContent = ''
+    
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                p1.textContent = data.error
+            } else{
+                p1.textContent = data.location
+                p2.textContent = data.forecast
+            }
+            
+        })
+    })
+})
